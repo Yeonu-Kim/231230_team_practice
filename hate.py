f4ca4510556a43aaeb82afcdcd5d30b22f09a1fb
yeonuKim <ywk0524@snu.ac.kr>
2023-12-30 12:43:14 +0900

    [feat] fix bug

--- hate.py
+++ hate.py
@@ -1,9 +1,7 @@
 def hate(like, cart_number):
     if like == True:
-        if cart_num == True:
+        if cart_number == True:
             cart_number[0] -= 1
             print(f'hate')
     else :
         cart_number[1] +=1
-
-	return 2
